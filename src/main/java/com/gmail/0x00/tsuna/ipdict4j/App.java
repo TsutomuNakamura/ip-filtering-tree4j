594a6302aa01901f5718756216482012d5e934fc
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2017-08-13 23:40:06 +0900

    Create new maven project

---
+++ src/main/java/com/gmail/0x00/tsuna/ipdict4j/App.java
@@ -0,0 +1,13 @@
+package com.gmail.0x00.tsuna.ipdict4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
